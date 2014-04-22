0e6bb5e8a182d75efff9488d69583b0ae88b4c74
thethp <todderick@gmail.com>
2014-04-22 04:05:41 -0400

    Some style changes and update to extension

---
+++ hulucastable_extension.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  $('.beacon-click').each(function(i, el) {
+    var videoBase = $(el).find('img')[0];
+    if(videoBase !== undefined) {
+      var videoIDStr = $(el).find('img').attr('src');
+      if(videoIDStr.indexOf('deo/') > 0) {
+        videoIDStr = videoIDStr.substring(videoIDStr.indexOf('deo/')+4,videoIDStr.indexOf('?'));
+        $(el).attr('href', '/stand_alone/'+videoIDStr);
+        $('.beacon-click .description').css('background-color','#A1CB39');
+      }
+    }
+  });
+}
